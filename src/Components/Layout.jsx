248f73dfebf67d7b2630252f49794ad23f79ce4f
Elie-Dev250 <iradukundaelie71@gmail.com>
2025-04-08 19:29:18 +0200

    pushing

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -5,11 +5,11 @@ const Layout = () => {
   return (
     <div>
       <nav className="navs">
-        <Link to="/page1">Page 1</Link>
-        <Link to="/page2">Page 2</Link>
-        <Link to="/page3">Page 3</Link>
-        <Link to="/page4">Page 4</Link>
-        <Link to="/page5">Page 5</Link>
+        <Link to="/page1">TodoList&counter-App</Link>
+        <Link to="/page2">Calculator-App</Link>
+        <Link to="/page3">Footers</Link>
+        <Link to="/page4">Signup-form</Link>
+        <Link to="/page5">Post</Link>
         
       </nav>
 
